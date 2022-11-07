5e8ea505090caf4d0c328849d138e8f9e14e761f
vividelax <115476584+vividelax@users.noreply.github.com>
2022-11-07 08:38:03 -0300

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,15 @@
+//exercicio 4
+var a = parseInt(prompt("digite um numero"))
+var b = parseInt(prompt("digite um numero"))
+
+if(a % b == 0){
+	console.log("São multiplos")
+}else{
+	console.log("Não são multiplos")
+}
+
+if(b % a == 0){
+	console.log("São multiplos")
+}else{
+	console.log("Não são multiplos")
+}
