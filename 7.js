9c229cbda1185a399f4bea6420c14b6797fb3828
vividelax <115476584+vividelax@users.noreply.github.com>
2022-11-07 08:38:30 -0300

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,9 @@
+random = Math.floor(Math.random() * 43) + 1;
+for(var i=1; i <= 44; i++){
+
+     console.log(1);
+     
+     if(i == random){
+     console.log("Estou entre "+ (i) +" e " + (i+1) );
+     }
+}
